01873d9591695438acc7906fa76cce4f7f512542
jasminko <betelge@gmail.com>
2018-01-22 17:50:39 +0700

    Hard-code mono but apply all other settings in shader

--- app/src/main/res/raw/skybox_f.glsl
+++ app/src/main/res/raw/skybox_f.glsl
@@ -7,13 +7,20 @@ varying vec2 uvCoord;
 uniform vec3 u_angles; // (hori angle (1=360), vert angle (1=180), verticalCrop)
 uniform sampler2D frame;
 
+vec4 equirectangular(sampler2D sampler, vec2 uv)
+{
+	uv -= vec2(.5);
+	uv /= u_angles.xy;
+	uv += vec2(.5);
+
+	// Crop
+	uv.y -= .5;
+	uv.y *= u_angles.z;
+	uv.y += .5;
+
+	return texture2D(sampler, uv);
+}
+
 void main(void) {
-    vec2 uv = uvCoord;
-
-    // Crop
-    uv.y -= .5;
-    uv.y *= u_angles.z;
-    uv.y += .5;
-
-    gl_FragColor = texture2D(sampler, uv);
+    gl_FragColor = equirectangular(frame, uvCoord);
 }
