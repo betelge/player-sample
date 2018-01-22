15def470a9ce7a6c63a665922d8c3f5ad8e28845
jasminko <betelge@gmail.com>
2018-01-22 17:48:50 +0700

    Hard-code 360 non-VR but apply crop setting in shader

--- app/src/main/res/raw/skybox_f.glsl
+++ app/src/main/res/raw/skybox_f.glsl
@@ -4,8 +4,16 @@ precision mediump float;
 
 varying vec2 uvCoord;
 
+uniform vec3 u_angles; // (hori angle (1=360), vert angle (1=180), verticalCrop)
 uniform sampler2D frame;
 
 void main(void) {
-    gl_FragColor = texture2D(frame, uvCoord);
+    vec2 uv = uvCoord;
+
+    // Crop
+    uv.y -= .5;
+    uv.y *= u_angles.z;
+    uv.y += .5;
+
+    gl_FragColor = texture2D(sampler, uv);
 }
