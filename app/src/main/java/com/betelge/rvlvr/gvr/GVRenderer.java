bc8c963cb0f0665bcc4638fd7b20b354b29b0be7
jasminko <betelge@gmail.com>
2018-01-24 13:12:06 +0700

    Use NEAREST filter for RGB texture

--- app/src/main/java/com/betelge/rvlvr/gvr/GVRenderer.java
+++ app/src/main/java/com/betelge/rvlvr/gvr/GVRenderer.java
@@ -117,8 +117,8 @@ public class GVRenderer implements GvrView.StereoRenderer, DriftRenderer {
         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, textureName);
         GLES20.glTexImage2D(GLES20.GL_TEXTURE_2D, 0, GLES20.GL_RGBA, width, height, 0,
                 GLES20.GL_RGBA, GLES20.GL_UNSIGNED_BYTE, null);
-        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_LINEAR);
-        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_LINEAR);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER, GLES20.GL_NEAREST);
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER, GLES20.GL_NEAREST);
 
         // YUV texture
         GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, yuvTextureName);
