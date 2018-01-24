30a2d701c189db4c2d563d0fa28cf271ae23f1a9
jasminko <betelge@gmail.com>
2018-01-24 22:04:58 +0700

    New color conversion for NV12

--- app/src/main/res/raw/converter_nv12_f.glsl
+++ app/src/main/res/raw/converter_nv12_f.glsl
@@ -14,16 +14,12 @@ void main(void) {
 
     y = texture2D(tex, v_texCoord).r;
     vec2 uv = texture2D(uvTex, v_texCoord).ra;
-    u = uv.x;
-    v = uv.y;
+    u = uv.x - .5;
+    v = uv.y - .5;
 
-    y = 1.1643 * (y - 0.0625);
-    u = u - 0.5;
-    v = v - 0.5;
-
-    r = y + 1.5958 * v;
-    g = y - 0.39173 * u - 0.81290 * v;
-    b = y + 2.017 * u;
+    r = y + 1.13983 * v;
+    g = y - 0.39465 * u - 0.58060 * v;
+    b = y + 2.03211 * u;
 
     gl_FragColor = vec4(r, g, b, 1.);
 }
